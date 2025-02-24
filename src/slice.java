2bd9dd202ffbcd378146ab015d3ffea396b3c304
rain-p <19291924738@162.com>
2025-02-24 12:31:49 +0800

    first commit

---
+++ src/slice.java
@@ -0,0 +1,32 @@
+ class slice {
+    
+    
+    
+        
+   
+        public String storename;
+        public String storeAddress;
+        public String storeName;
+        public String storePhone;
+        public String storeMenu;
+        public String pizzaIngredients;
+        public double pizzaPrice;
+        public double sides;
+        public String drinks;
+        public int orderID;
+        public int orderTotal;
+        public void otakeOrder(){
+            System.out.println(9);
+
+        };
+        public void makePizza()
+        {
+
+        };
+        public void printReceipt()
+        {
+           System.out.println("orderID:"+orderID);
+        }
+    }
+
+
